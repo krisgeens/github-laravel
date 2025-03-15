f62e2f82ad80a653eee808a981d2b5488bbcfead
Kris Geens <krisgeens@hotmail.com>
2025-03-16 00:41:00 +0100

    afgewerkt: oefening 01a

---
+++ 01a_oefening_basisrouting/resources/views/wezens/show.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wezens</title>
+</head>
+<body>
+    <h1>wezens: {{ $wezen }}</h1>
+</body>
+</html>
